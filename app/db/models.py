fb588f6e53a93f058d0884937b3fcbff03820f51
mojtaba211 <103147690+mojtaba211@users.noreply.github.com>
2025-10-07 18:01:16 +0330

    username len : 32-->36

--- app/db/models.py
+++ app/db/models.py
@@ -151,7 +151,7 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True)
-    username = Column(String(32), unique=True, index=True)
+    username = Column(String(36), unique=True, index=True)
     key = Column(String(64), unique=True)
     activated = Column(Boolean, nullable=False, default=True)
     enabled = Column(
